972a40c62ca4e25d113d2f1c5fc62aeeeee15a51
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-03-05 11:20:04 +0000

    do arrow functions tutorial

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,42 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(2, 6);
+console.log(sum);
+
 
 // Arrow Function With Parameters
+const addThreeNumbers = (a, b, c) => {
+    return a + b + c;
+}
+
+let plus = addThreeNumbers(1, 5, 2);
+console.log(plus)
+
 
 // Single Line Arrow Function With Parameters
+const addFourNumbers = (a, b, c, d) => a + b + c + d;
+let addition = addFourNumbers(2, 2, 2, 2)
+console.log(addition)
+
 
 // Implicit Returns
+const saySomething = message => console.log(message)
+saySomething("Hello World!")
+
+const sayHello = () => console.log("Hello")
+sayHello();
+
 
 // Returning Multiple Lines
+const returnMultipleLines = () =>   (
+    `<p>
+        This is a multiline strong!
+    </p>`
+)
+
+console.log(returnMultipleLines());
